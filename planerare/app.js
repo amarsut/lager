80c1f2e22a2e5cefa875cfcd11204579ba300e0c
Amar Sut <amar.sut@gmail.com>
2025-12-01 23:14:03 +0100

    Update app.js

--- planerare/app.js
+++ planerare/app.js
@@ -1395,7 +1395,7 @@
 			                });
 
 							const spacer = document.createElement('div');
-								spacer.style.height = "47px"; // Tvinga 100px tomrum i botten
+								spacer.style.height = "50px"; // Tvinga 100px tomrum i botten
 								spacer.style.flexShrink = "0"; // Se till att den inte krymper
 								chatList.appendChild(spacer);
 			
