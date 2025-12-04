ac05536c8c5427f81803ba55d44df62f21bffdd4
Amar Sut <amar.sut@gmail.com>
2025-12-04 09:33:58 +0100

    Update app.js

--- planerare/app.js
+++ planerare/app.js
@@ -2008,7 +2008,7 @@
 				            // Detta tvingar listan att ha mycket luft i botten så sista meddelandet
 				            // och dess tidsstämpel hamnar OVANFÖR din textruta.
 				            const spacer = document.createElement('div');
-				            spacer.style.height = "140px"; // <-- ÄNDRAD TILL 140px
+				            spacer.style.height = "90px"; // <-- ÄNDRAD TILL 140px
 				            spacer.style.flexShrink = "0"; 
 				            chatList.appendChild(spacer);
 				
