c42b7ae455e3e68dba8a21a68ba98fa78ce4c703
Amar Sut <amar.sut@gmail.com>
2025-12-06 02:30:50 +0100

    Update config.js

--- planerare/config.js
+++ planerare/config.js
@@ -1,5 +1,4 @@
 const CONFIG = {
     AI_API_KEY: "AIzaSyAiJsl5jBp_TaQlXlXKsTxvW-RFNd5OnUg"
-    AI_GEMINI_LINK: "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}"
 };
 //gammal AIzaSyAiJsl5jBp_TaQlXlXKsTxvW-RFNd5OnUg
