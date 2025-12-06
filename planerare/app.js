a0f37e313f3b6b636a0052d7bb0ceb666c559c4d
Amar Sut <amar.sut@gmail.com>
2025-12-06 01:32:55 +0100

    Update app.js

--- planerare/app.js
+++ planerare/app.js
@@ -7440,7 +7440,8 @@
 	
 	    try {
 	        // 2. Proxy-anrop (Hämta rådata)
-	        const targetUrl = `https://biluppgifter.se/fordon/${regnr}`;
+	        //const targetUrl = `https://biluppgifter.se/fordon/${regnr}`;
+			const targetUrl = `https://www.car.info/sv-se/license-plate/S/${regnr}/specs`;
 	        const proxyUrl = `https://corsproxy.io/?${encodeURIComponent(targetUrl)}`;
 	        const response = await fetch(proxyUrl);
 	
