5bf59b3c65ade1bb842650192d86166e726336bd
Amar Sut <amar.sut@gmail.com>
2025-12-06 01:01:59 +0100

    Update app.js

--- planerare/app.js
+++ planerare/app.js
@@ -2267,7 +2267,7 @@
 			            chatInput.value = '';
 			
 			            try {
-			                const apiKey = "AIzaSyAiJsl5jBp_TaQlXlXKsTxvW-RFNd5OnUg"; //https://aistudio.google.com/app/api-keys
+							const apiKey = CONFIG.AI_API_KEY;
 			                const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
 			
 			                const prompt = `Du är en expertmekaniker. Svara på svenska.
@@ -7358,7 +7358,7 @@
 	        `;
 	
 	        // 1. RÄTTAD NYCKEL (Den var avklippt i din kod)
-			const apiKey = "AIzaSyAiJsl5jBp_TaQlXlXKsTxvW-RFNd5OnUg"; 
+			const apiKey = CONFIG.AI_API_KEY;
 
             // HÄR ÄR MODELL-ÄNDRINGEN (Vi kör på säkra 1.5-flash):
             const aiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
@@ -7496,7 +7496,7 @@
 	        `;
 
             // KLISTRA IN DIN NYA API-NYCKEL HÄR:
-            const apiKey = "AIzaSyAiJsl5jBp_TaQlXlXKsTxvW-RFNd5OnUg"; 
+			const apiKey = CONFIG.AI_API_KEY;
 
             // HÄR ÄR MODELL-ÄNDRINGEN (Vi kör på säkra 1.5-flash):
             const aiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
