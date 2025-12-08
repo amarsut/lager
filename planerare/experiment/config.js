bc17d52b41ff3b446d4ecbb015ba399f937aaab1
Amar Sut <amar.sut@gmail.com>
2025-12-09 00:01:28 +0100

    Update config.js

--- planerare/experiment/config.js
+++ planerare/experiment/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    AI_API_KEY: "AIzaSyAiJsl5jBp_TaQlXlXKsTxvW-RFNd5OnUg"
+    AI_API_KEY: "AIzaSyC_NX_47LNn8zcNlkG3fZqnSabbbtIwv00"
 };
 //gammal AIzaSyAiJsl5jBp_TaQlXlXKsTxvW-RFNd5OnUg
