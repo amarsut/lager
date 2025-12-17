4f0a87c196e3a25869c806dd605bb8926678ee42
Amar Sut <amar.sut@gmail.com>
2025-12-17 14:12:33 +0100

    Update app.js

--- planerare/app.js
+++ planerare/app.js
@@ -760,20 +760,13 @@ function updateStatsCounts(jobs) {
     // Uppdatera "Ej Bokat"-kortet
     safeUpdate('stat-backlog', unplannedCount + offeredCount);
 
+    // Hitta raden och lägg in enkel, ren data
     const backlogRow = document.querySelector('.backlog-row');
     if (backlogRow) {
         backlogRow.innerHTML = `
-            <div class="stat-inline-row">
-                <span class="sub-group">
-                    <span class="sub-label">VÄNTAR</span>
-                    <b class="sub-val">${unplannedCount}</b>
-                </span>
-                <span class="sub-separator">&bull;</span>
-                <span class="sub-group">
-                    <span class="sub-label">OFFERT</span>
-                    <b class="sub-val">${offeredCount}</b>
-                </span>
-            </div>
+            <span class="sub-stat"><b class="sub-val">${unplannedCount}</b> väntar</span>
+            <span class="sub-spacer"></span>
+            <span class="sub-stat"><b class="sub-val">${offeredCount}</b> offert</span>
         `;
     }
 
