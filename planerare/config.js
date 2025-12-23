399378181f041d518c7da075fd33e1a721dd6240
Amar Sut <amar.sut@gmail.com>
2025-12-23 14:58:52 +0100

    Update config.js

--- planerare/config.js
+++ planerare/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    AI_API_KEY: "AIzaSyCmlhKoKEMl_XInIimngXUlheKiGFiqEWU"
+    AI_API_KEY: "AIzaSyB3Z_VrLt9YlRlSXpMXBbzYtII6j17ZqF4"
 };
 //gammal AIzaSyAiJsl5jBp_TaQlXlXKsTxvW-RFNd5OnUg
