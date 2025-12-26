c3be51b39e99ed611ae3e2003d5a17a94f4f65d2
Amar Sut <amar.sut@gmail.com>
2025-12-26 19:15:02 +0100

    Update lager-view.js

--- planerare/lager-view.js
+++ planerare/lager-view.js
@@ -463,7 +463,12 @@ export function renderEliteTable(items) {
                             </a>
                         `).join('')}
                     </div>
-                </div>`;
+                <div class="expand-chevron-container">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
+                        <polyline points="6 9 12 15 18 9"></polyline>
+                    </svg>
+                </div>
+            </div>`;
         }).join('');
     }
 
