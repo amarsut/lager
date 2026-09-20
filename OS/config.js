8c978eae9a5b3f4898532b37da5b0da04bc01403
Amar Sut <amar.sut@gmail.com>
2026-09-20 20:02:32 +0200

    Update config.js to fix syntax and log ENV
    Fix syntax and add console log for environment variable loading.

--- OS/config.js
+++ OS/config.js
@@ -1,4 +1,4 @@
-// config.js
 window.ENV = {
-    GEMINI_API_KEY: "AQ.Ab8RN6LcG1Qh5MXoCXYp67WBdqIojpO38EFj9yGqU88JDablnA"
-};
+      GEMINI_API_KEY: "AQ.Ab8RN6LcG1Qh5MXoCXYp67WBdqIojpO38EFj9yGqU88JDablnA"
+  };
+  console.log("ENV laddad:", window.ENV);
