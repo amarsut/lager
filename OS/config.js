5fee9f4a168b5f933ccb6ec6a09be06ca9e8267e
Amar Sut <amar.sut@gmail.com>
2026-09-20 19:54:40 +0200

    Update GEMINI_API_KEY in config.js

--- OS/config.js
+++ OS/config.js
@@ -1,4 +1,4 @@
 // config.js
 window.ENV = {
-    GEMINI_API_KEY: "AQ.Ab8RN6KW0m01UcGiAurTS9i8_Usn9I1mBXPdij3ltwwJ5bcB-A"
+    GEMINI_API_KEY: "AQ.Ab8RN6LcG1Qh5MXoCXYp67WBdqIojpO38EFj9yGqU88JDablnA"
 };
